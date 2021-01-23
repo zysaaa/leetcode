ba4d3ad1db05c34c16ea8ee4b5d88bbb4bef118f
zysaaa <982020642@qq.com>
2021-01-23 15:47:13 +0800

    SortedArrayToBST

---
+++ src/java/com/zysaaa/zysaaa20210123/Tree2str.java
@@ -0,0 +1,31 @@
+//package com.zysaaa.zysaaa20210123;
+//
+///**
+// * @description: 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+// *
+// * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+// *
+// * 来源：力扣（LeetCode）
+// * 链接：https://leetcode-cn.com/problems/construct-string-from-binary-tree
+// * @author:zysaaa
+// * @date: 2021/1/23 13:22
+// */
+//public class Tree2str {
+//
+//    public String tree2str(TreeNode t) {
+//
+//    }
+//
+//
+//    static class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//        TreeNode(int x) { val = x; }
+//        TreeNode(int val, TreeNode left, TreeNode right) {
+//            this.val = val;
+//            this.left = left;
+//            this.right = right;
+//        }
+//    }
+//}
