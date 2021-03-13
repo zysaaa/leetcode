7c99a261112c3a82116e273a0f3b1787a4136dcc
zysaaa <982020642@qq.com>
2021-03-13 19:04:55 +0800

    TreeDepth.java

---
+++ src/java/com/zysaaa/zysaaa20210225/TreeDepth.java
@@ -0,0 +1,20 @@
+package com.zysaaa.zysaaa20210225;
+
+import com.zysaaa.common.TreeNode;
+
+/**
+ * @description: https://www.nowcoder.com/practice/435fb86331474282a3499955f0a41e8b?tpId=13&tqId=11191&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking&tab=answerKey
+ * @author:zysaaa
+ * @date: 2021/3/13 18:28
+ */
+public class TreeDepth {
+    public int TreeDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        // fuck easy
+
+    }
+}
