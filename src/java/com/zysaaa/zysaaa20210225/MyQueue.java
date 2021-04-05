903e7a6a3d29be04b8fa815ff20694d41f937bbb
zysaaa <982020642@qq.com>
2021-04-05 18:31:35 +0800

    MyQueue

---
+++ src/java/com/zysaaa/zysaaa20210225/MyQueue.java
@@ -0,0 +1,52 @@
+package com.zysaaa.zysaaa20210225;
+
+import java.util.Stack;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * @description: https://leetcode-cn.com/problems/implement-queue-using-stacks/
+ * @author:zysaaa
+ * @date: 2021/4/5 18:00
+ */
+public class MyQueue {
+
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack2.isEmpty();
+    }
+
+}
