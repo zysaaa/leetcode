ec081560af7083710f0646252537cd92b53d39ba
undefined <undefined>
2021-04-11 15:29:42 +0800

    Intersection.java

---
+++ src/java/com/zysaaa/zysaaa20210225/Intersection222.java
@@ -0,0 +1,25 @@
+package com.zysaaa.zysaaa20210225;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Intersection222 {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> result = new HashSet<>();
+        int i = 0;
+        int j = 0;
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        while (true) {
+
+
+
+
+
+        }
+    }
+}
